c4acaa5ae6d951d285413c838a6c6a5ae10ef65f
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-17 02:19:41 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,27 +1,21 @@
 class PigLatinizer
 
+  attr_accessor :user_phrase
+
   def piglatinize(word)
-    return word if %w[and an in].include?(word) #one syllable exceptions
-    letters = word.split("")
-    letters.keep_if {|letter| letter != "."}
-    if letters.size > 1
-      until vowel?(letters[0])
-        letters << letters.shift
-      end
-      letters  << "ay"
+    if word.downcase.index(/[aeiou]/) == 0
+      word + "way"
+    else
+      vowel_index = word.index(/[aeiou]/)
+      front_end = word.slice!(0..vowel_index-1)
+      word + front_end +"ay"
     end
-    letters.join
   end
 
-  def to_pig_latin(text)
-    words = text.split(" ")
-    words.map! {|word| piglatinize(word)}
-    words.join(" ")
+  def piglatinize_helper(phrase)
+    array = phrase.split(" ")
+    @user_phrase = array.map {|word| piglatinize(word)}.join(" ")
   end
 
-  def vowel?(letter)
-    letter.downcase
-    letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
-  end
 
 end
