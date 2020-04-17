f28470d0b82dee778e349ab2a8191bc8bd787ae4
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-17 02:35:27 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,7 +9,12 @@ class PigLatinizer
   end
 
   def to_piglatin(phrase)
+    first_letter = word[0].downcase
+  if ["a", "e", "i", "o", "u"].include?(first_letter)
+      "#{word}way"
+  else
 
+  end
   end
 
 
