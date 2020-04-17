d0f1041016eaac31c53b4a76cd858cf004744d2b
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-17 02:40:10 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -19,7 +19,7 @@ class PigLatinizer
       consonants << word[1]
     if ["a", "e", "i", "o", "u"].include?(word[2]) == false
       consonants << word[2]
-       end
+        end
       end
       "#{word[consonants.length..-1] + consonants.join + "ay"}"
     end
