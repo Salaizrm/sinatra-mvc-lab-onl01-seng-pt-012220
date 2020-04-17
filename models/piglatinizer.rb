b890ecf5c0e869027a92b68f3d830f421fa75948
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-17 02:01:29 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,8 +1,9 @@
 class PigLatinizer
 
   def piglatinize(phrase)
-
-
+    a = string.split(" ")
+        b = a.map {|word| piglatinize_word(word)}
+        b.join(" ")
   end
 
   def word_splitter(word)
