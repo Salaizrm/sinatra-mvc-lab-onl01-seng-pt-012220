280679a6ca6f6d67136f1bae0b8721315c745369
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-17 02:18:24 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,18 +1,21 @@
 class PigLatinizer
-  attr_accessor :piglatinized_phrase
 
-def piglatinize(word) #pig latinizes a single word
-  if word.downcase.index(/[aeiou]/) == 0
-    word + "way"
-  else
-    vowel_index = word.index(/[aeiou]/)
-    front_end = word.slice!(0..vowel_index-1)
-    word + front_end +"ay"
+  attr_accessor :user_phrase
+
+  def piglatinize(word)
+    if word.downcase.index(/[aeiou]/) == 0
+      word + "way"
+    else
+      vowel_index = word.index(/[aeiou]/)
+      front_end = word.slice!(0..vowel_index-1)
+      word + front_end +"ay"
+    end
   end
-end
 
-def to_pig_latin(phrase)
-  word_array = phrase.split(" ")
-  @piglatinized_phrase = word_array.collect {|word| piglatinize(word)}.join(" ")
+  def piglatinize_helper(phrase)
+    array = phrase.split(" ")
+    @user_phrase = array.map {|word| piglatinize(word)}.join(" ")
+  end
+
+
 end
-end #class end
