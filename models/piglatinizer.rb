469e51567a9dba63a201e604036494ab3ff9b3c9
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-17 02:03:35 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,6 +7,11 @@ class PigLatinizer
   end
 
   def word_splitter(word)
+    first_letter = word[0].downcase
+      if first_letter == "a" || first_letter == "e" || first_letter == "i" || first_letter == "o" || first_letter == "u"
+    else
+    # piglatinize word that starts with a consonant
+end
   end
 
 end
